7d077b9a1a33bc23c462021e0fa8f910a164c0c1
Jay Freeman <byronbayer@gmail.com>
2019-08-12 00:42:12 +0100

    Added Switch AzureRm with Az

---
+++ Switch-AzureRmWithAz.ps1
@@ -0,0 +1,35 @@
+function Switch-AzureRmWithAz {
+    <#
+    .SYNOPSIS
+        Replace AzureRm with Az
+    .DESCRIPTION
+        This powershell script will remove AzureRm modue and replace it with the new Az module
+    .EXAMPLE
+        PS C:\> Switch-AzureRmWithAz    
+    .NOTES
+        General notes
+    #>
+
+    $azureRmModule = Get-InstalledModule AzureRM
+
+    if ($azureRmModule) {
+        Write-Host 'AzureRM module exists. Removing it'
+        Uninstall-Module -Name AzureRM -AllVersions
+        Write-Host 'AzureRM module removed'
+    }
+    else {
+        Write-Host '* Congratulations * !!!! AzureRM module does not exist.'
+    }
+    $azModule = Get-InstalledModule Az
+
+    if ($azModule) {
+        'Updating Az module to the latest version'
+        Update-Module Az
+    }
+    else {
+        'Installing the Az module'
+        Install-Module Az -Force -confirm:$false -AllowClobber
+        Uninstall-AzureRm
+    }
+}
+Switch-AzureRmWithAz
