b90aff4c708d85994ee5c7503921fb102145d36d
Jay Freeman <byronbayer@gmail.com>
2019-10-03 11:26:27 +0100

    Update to find files scripts

--- ListFoldersOnAllServers/FindFilesModifiedWithinDate.ps1
+++ ListFoldersOnAllServers/FindFilesModifiedWithinDate.ps1
@@ -3,16 +3,11 @@
 #Get-WmiObject Win32_DiskDrive
 
 $Drives = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 }
-#$dateString = (Get-Date).ToString("yyyy_MM_dd_HH_mm_ss")
 
 foreach ($Drive in $Drives) {
     $currentDrive = $Drive.DeviceId + "\"
-    $DriveLetter = $currentDrive -replace ":", ""
-    #$LogFileName = "$env:COMPUTERNAME-$DriveLetter-$dateString.txt"
-    $Files = Get-ChildItem -Path $currentDrive -Recurse -ErrorAction Continue | `
+    
+    Get-ChildItem -Path $currentDrive -Recurse -ErrorAction Continue | `
             Where-Object { $_.LastWriteTime -gt (Get-Date).AddDays(-1) } | `
             Select-Object Directory -Unique
-    
-    $Files
-    
 }
--- ListFoldersOnAllServers/RunPsScriptOnAllMachines.ps1
+++ ListFoldersOnAllServers/RunPsScriptOnAllMachines.ps1
@@ -1,3 +1,2 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force -Confirm:$false
-
-$result = Invoke-Command -ComputerName (Get-Content C:\Powershell\Servers.txt) -FilePath C:\Powershell\GetModifiedFiles.ps1
+Invoke-Command -ComputerName (Get-Content C:\Powershell\Servers.txt) -FilePath C:\Powershell\GetModifiedFiles.ps1
