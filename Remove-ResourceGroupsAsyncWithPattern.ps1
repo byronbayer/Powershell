a246ce8b6e08afe79c15490ec0a0212803bbe971
byronbayer <byronbayer@gmail.com>
2020-01-01 10:17:31 +0000

    Added removal of locks

--- Remove-ResourceGroupsAsyncWithPattern.ps1
+++ Remove-ResourceGroupsAsyncWithPattern.ps1
@@ -22,7 +22,11 @@
         #An array of resource group patterns
         [Parameter(Mandatory = $true)]
         [string[]]
-        $ResourceGroupNames
+        $ResourceGroupNames,
+        # Parameter help description
+        [Parameter(Mandatory = $false)]
+        [bool]
+        $RemoveLocks = $false
                
     )
     $allResourceGroups = Get-AzResourceGroup
@@ -30,7 +34,14 @@
         "Current Resouces Group Name: $ResoucesGroupName"
         $resourceGroups = $allResourceGroups | Where-Object ResourceGroupName -Like $ResoucesGroupName
         foreach -parallel ($resourceGroup in $resourceGroups) {
-            $ResourceGroupName = $resourceGroup.ResourceGroupName        
+            $ResourceGroupName = $resourceGroup.ResourceGroupName
+            
+            if ($RemoveLocks) {
+                'Removing any Locks from resources in ' + $ResourceGroupName
+                Get-AzResourceLock | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName } | Remove-AzResourceLock -Force
+                
+            }
+
             $df = Get-AzDataFactoryV2 -ResourceGroupName $ResourceGroupName
             if ($df) {
                 $ir = Get-AzDataFactoryV2IntegrationRuntime -ResourceGroupName $ResourceGroupName -DataFactoryName $df.DataFactoryName
@@ -41,6 +52,7 @@
                     }
                 }
             }
+                        
             'Removing resource group ' + $ResourceGroupName
             Remove-AzResourceGroup -Id $resourceGroup.ResourceId -Force -Confirm:$false
         }
@@ -52,8 +64,10 @@ function Remove-ResourceGroupsAsync {
         #An array of resource group patterns
         [Parameter(Mandatory = $true)]
         [string[]]
-        $ResourceGroupNamePatterns
-               
+        $ResourceGroupNamePatterns,
+        [Parameter(Mandatory = $false)]
+        [bool]
+        $RemoveLocks = $false
     )
     $allResourceGroups = Get-AzResourceGroup    
     $collection = { $ResourceGroupNamePatterns }.Invoke()
@@ -70,17 +84,70 @@ function Remove-ResourceGroupsAsync {
 
     $confirmation = Read-Host "Are you Sure You Want To delete the above resouce groups (y/n)?"
     if ($confirmation -eq 'y') {
-        Remove-ResourcegroupsInParralell -ResourceGroupNames $ResourceGroupNamePatterns
+        Remove-ResourcegroupsInParralell -ResourceGroupNames $ResourceGroupNamePatterns -RemoveLocks $RemoveLocks
     }
     else {
         Write-Host "Resource groups not deleted"
     }
 }
 
+function Add-CurrentIpToServerFirewall {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceGroupName,
+        [Parameter(Mandatory = $true)]
+        [string] $ServerName
+    )
+
+    $ip = Invoke-RestMethod http://ipinfo.io/json | Select-Object -exp ip
+    $firewallRuleName = "AllowIpForDeployment"
+    New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroupName `
+        -ServerName $ServerName `
+        -FirewallRuleName $firewallRuleName `
+        -StartIpAddress $ip -EndIpAddress $ip -Verbose
+
+}
+
+
+# ################################ Set up test data ############################################
+
+# $resourceGroupeName = 'my-rg-001'
+# $location = 'UK South'
+
 # for ($i = 1; $i -lt 10; $i++) {
-#     New-AzResourceGroup -Name "my-rg-00$i" -Location 'UK South' -Confirm:$false -Force | Out-Null
-#     New-AzResourceGroup -Name "test-rg-00$i" -Location 'UK South' -Confirm:$false -Force | Out-Null
+#     New-AzResourceGroup -Name "my-rg-00$i" -Location $location -Confirm:$false -Force | Out-Null
+#     New-AzResourceGroup -Name "test-rg-00$i" -Location $location -Confirm:$false -Force | Out-Null
 # }
 
+# $cred = $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'AdminUser', $(ConvertTo-SecureString -String 'p@$$w0rd' -AsPlainText -Force))
+# $sqlServer = New-AzSqlServer -ServerName 'my-svr-001' -Location $location -ResourceGroupName $resourceGroupeName -SqlAdministratorCredentials $cred
+# Add-CurrentIpToServerFirewall -ResourceGroupName $resourceGroupeName -ServerName $sqlServer.ServerName
+# New-AzSqlServerFirewallRule -ResourceGroupName $resourceGroupeName -ServerName $sqlServer.ServerName -AllowAllAzureIPs
+
+# $adf = Set-AzDataFactoryV2 -Name 'my-df-001' -Location $location -ResourceGroupName $resourceGroupeName
+# $dfrt = Set-AzDataFactoryV2IntegrationRuntime -ResourceGroupName $resourceGroupeName `
+#     -Name 'integrationRuntime' `
+#     -Location $location `
+#     -DataFactoryName $adf.DataFactoryName `
+#     -CatalogServerEndpoint $sqlServer.FullyQualifiedDomainName `
+#     -CatalogAdminCredential $cred `
+#     -Type Managed `
+#     -NodeSize 'Standard_D2_v3' `
+#     -NodeCount 1 `
+#     -CatalogPricingTier 'S0' `
+#     -MaxParallelExecutionsPerNode 1
+
+# ### This can take 20 minutes to provision ###
+# Start-AzDataFactoryV2IntegrationRuntime -ResourceId $dfrt.Id -Force -Verbose
+# $asp = New-AzAppServicePlan -Name 'my-asp-001' -Location $location -ResourceGroupName $resourceGroupeName
+
+# New-AzResourceLock -ResourceGroupName $resourceGroupeName -LockName 'my-asp-001-lock' -LockLevel CanNotDelete -ResourceName $asp.Name -ResourceType 'Microsoft.Web/serverfarms' -Force -Confirm:$false 
+
+##############################################################################################
+
+# $AzCredential = $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'jay.freeman@purplebricks.com', $(ConvertTo-SecureString -String '7XZ$0*hp1cDrkVzvgXkiRMNTs' -AsPlainText -Force))
+# Connect-AzAccount -Credential $AzCredential
+# Select-AzSubscription 'Dev-Playground'
+
 $ResourceGroupNamePatterns = "my*", "test*"
-Remove-ResourcegroupsAsync -ResourceGroupNamePatterns $ResourceGroupNamePatterns
+Remove-ResourcegroupsAsync -ResourceGroupNamePatterns $ResourceGroupNamePatterns -RemoveLocks $true
