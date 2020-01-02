d2a37d66019d3ac4f784383b6e9d47bd7542fae7
byronbayer <byronbayer@gmail.com>
2020-01-02 01:48:25 +0000

    Added all subscriptions to Remove Deployments script

--- Remove Deployments greater then x days.ps1
+++ Remove Deployments greater then x days.ps1
@@ -7,25 +7,36 @@
         $Days = 0,
         [Parameter(Mandatory = $false)]
         [bool]
-        $ShowOnlyCounts = $false        
+        $ShowOnlyCounts = $false,
+        [Parameter(Mandatory = $false)]
+        [bool]
+        $ShowAllResourceGroups = $false
     )
-    $totalCount = 0
-    Get-AzResourceGroup | ForEach-Object {
-        "Resource group: " + $_.ResourceGroupName
-        $count = 0
-        Get-AzResourceGroupDeployment -ResourceGroupName $_.ResourceGroupName | 
-        Where-Object { $_.Timestamp.Date -lt ((Get-Date).ToUniversalTime().AddDays(-$Days)) } | 
-        ForEach-Object {
-            if (!$ShowOnlyCounts) {
-                'Deleteing ' + $_.DeploymentName + ' ' + $_.Timestamp            
-                Remove-AzResourceGroupDeployment -Name $_.DeploymentName -ResourceGroupName $_.ResourceGroupName
-            }            
-            $count ++
-        } 
-        $count
-        $totalCount = $totalCount + $count
+    Get-AzSubscription | ForEach-Object {
+        $subscriptionName = $_.Name
+        "---------------------------- $subscriptionName -----------------------------"
+        Select-AzSubscription $_.SubscriptionId | Out-Null
+        $totalCount = 0
+        Get-AzResourceGroup | ForEach-Object {
+            $count = 0
+            Get-AzResourceGroupDeployment -ResourceGroupName $_.ResourceGroupName | 
+            Where-Object { $_.Timestamp.Date -lt ((Get-Date).ToUniversalTime().AddDays(-$Days)) } | 
+            ForEach-Object {
+                if (!$ShowOnlyCounts) {
+                    'Deleteing ' + $_.DeploymentName + ' ' + $_.Timestamp            
+                    Remove-AzResourceGroupDeployment -Name $_.DeploymentName -ResourceGroupName $_.ResourceGroupName
+                }            
+                $count ++
+            }
+            if ($ShowAllResourceGroups -or $count -gt 0) {
+                "Resource group: " + $_.ResourceGroupName
+                $count
+            }
+            $totalCount = $totalCount + $count
+        }
+        "Total Count for $subscriptionName : $totalCount"
+        "---------------------------------------------------"
     }
-    $totalCount
 }
 $Days = 0
-Remove-OldDeployments -Days $Days -ShowOnlyCounts $true -WhatIf
+Remove-OldDeployments -Days $Days -ShowOnlyCounts $true -ShowAllResourceGroups $false
