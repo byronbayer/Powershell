1754d4dc424ed44028a5017ab8fa4485f9b0fb47
byronbayer <byronbayer@gmail.com>
2020-01-02 01:52:20 +0000

    GetVmDetails for all subscriptions

--- GetVmDetails.ps1
+++ GetVmDetails.ps1
@@ -1,10 +1,13 @@
-
-$VM = Get-AzVM
-$VM | Select-Object ResourceGroupName, Name, Location, `
-    @{Name = "VmSize"; Expression = { $_.HardwareProfile.VmSize } }, `
-    @{Name = "OSType"; Expression = { $_.StorageProfile.OSDisk.OSType } }, `
-    @{Name = "NIC"; Expression = { (Get-AzNetworkInterface -ResourceId $_.NetworkProfile.networkInterfaces.Id).Name } }, `
-    @{Name = "VNET"; Expression = { ((Get-AzNetworkInterface -ResourceId $_.NetworkProfile.networkInterfaces.Id).IpConfigurations[0].Subnet.Id -replace "/subnets.*", "") -replace ".*virtualNetworks/", "" } }, `
-    @{Name = "SubNet"; Expression = { (Get-AzNetworkInterface -ResourceId $_.NetworkProfile.networkInterfaces.Id).IpConfigurations[0].Subnet.Id -replace ".*subnets/", "" } }, `
-    ProvisioningState, `
-    Zones | Format-Table
+$Subscriptions = Get-AzSubscription
+foreach ($Subscription in $Subscriptions) {
+    Select-AzSubscription $Subscription.SubscriptionId
+    $VM = Get-AzVM
+    $VM | Select-Object ResourceGroupName, Name, Location, `
+        @{Name = "VmSize"; Expression = { $_.HardwareProfile.VmSize } }, `
+        @{Name = "OSType"; Expression = { $_.StorageProfile.OSDisk.OSType } }, `
+        @{Name = "NIC"; Expression = { (Get-AzNetworkInterface -ResourceId $_.NetworkProfile.networkInterfaces.Id).Name } }, `
+        @{Name = "VNET"; Expression = { ((Get-AzNetworkInterface -ResourceId $_.NetworkProfile.networkInterfaces.Id).IpConfigurations[0].Subnet.Id -replace "/subnets.*", "") -replace ".*virtualNetworks/", "" } }, `
+        @{Name = "SubNet"; Expression = { (Get-AzNetworkInterface -ResourceId $_.NetworkProfile.networkInterfaces.Id).IpConfigurations[0].Subnet.Id -replace ".*subnets/", "" } }, `
+        ProvisioningState, `
+        Zones | Format-Table    
+}
